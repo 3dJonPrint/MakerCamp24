391acf7bd847810fd9ab3cfff0d9f259bc66a79a
3dJonPrint <jonas.spitzmueller@outlook.de>
2024-08-22 15:34:43 +0200

    runing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyxboxcontroller import XboxController, XboxControllerState
 import gpiozero
+import funktionen as fu
 
 controller = XboxController(0)
 
@@ -8,5 +9,5 @@ while True:
 
     left_stick_x: float = state.l_thumb_x
     left_stick_y: float = state.l_thumb_y
-
+    fu.dirve(left_stick_x,left_stick_y)
     print(left_stick_x, left_stick_y)
