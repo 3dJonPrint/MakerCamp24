964d0f97a32c065be1d39ea68cb03772b40873d0
3dJonPrint <jonas.spitzmueller@outlook.de>
2024-08-23 10:09:48 +0200

    dümmster fehler v2

--- pins.py
+++ pins.py
@@ -1,19 +1,19 @@
 import gpiozero as gpio
 
 #motor pins
-rev_left_pin = 5
-for_left_pin = 6
+for_left_pin = 5
+rev_left_pin = 6
 speed_left_pin = 13
 
 for_right_pin = 14
 rev_right_pin = 15
 speed_right_pin = 18
 
+#motor pins initalisirung
 for_right = gpio.DigitalOutputDevice(for_right_pin)
-rev_right = gpio.DigitalInputDevice(rev_right_pin)
+rev_right = gpio.DigitalOutputDevice(rev_right_pin)
 speed_right = gpio.PWMOutputDevice(speed_right_pin)
 
-#motor pins initalisirung
 for_left = gpio.DigitalOutputDevice(for_left_pin)
 rev_left = gpio.DigitalOutputDevice(rev_left_pin)
 speed_left = gpio.PWMOutputDevice(speed_left_pin)
